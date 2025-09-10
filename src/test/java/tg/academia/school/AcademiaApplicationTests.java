f7b8f4f18af7ca56f5419d2c8e1e33b65c76c8c1
protic-togo <bernard@adanlessossi.com>
2025-09-10 21:40:20 +0200

    First Commit

---
+++ src/test/java/tg/academia/school/AcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package tg.academia.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
